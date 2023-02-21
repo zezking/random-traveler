68a0e859ded6af2096674e660974e8b2cfd9eb22
Enze Zhao <zhaoenze001@gmail.com>
2023-02-20 17:43:16 -0800

    Add user cities and add associate users with cities api test

---
+++ server/app/Http/Controllers/UserCitiesController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UserCities;
+use Illuminate\Http\Request;
+
+class UserCitiesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\UserCities  $userCities
+     * @return \Illuminate\Http\Response
+     */
+    public function show(UserCities $userCities)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\UserCities  $userCities
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(UserCities $userCities)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\UserCities  $userCities
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, UserCities $userCities)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\UserCities  $userCities
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(UserCities $userCities)
+    {
+        //
+    }
+}
--- server/routes/api.php
+++ server/routes/api.php
@@ -20,14 +20,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
-Route::post('tokens/create', function (Request $request){
-    $token = $request->user()->createToken($request->token_name);
-
-    return ['token' => $token->plainTextToken];
-});
-
 Route::post('/auth/register', [AuthController::class, 'createUser']);
 Route::middleware('auth:sanctum')->post('/auth/login', [AuthController::class, 'loginUser']);
-Route::middleware('auth:sanctum')->post('/auth/expires_token', [AuthController::class, 'expiresUserToken']);
+Route::middleware('auth:sanctum')->post('/auth/logout', [AuthController::class, 'expiresUserToken']);
 
